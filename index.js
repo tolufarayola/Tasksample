98048aa87e84feda685dea5fcc21c2103eafbec1
tolufarayola <105269763+tolufarayola@users.noreply.github.com>
2022-06-25 05:40:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let firstname = 'Tolulope';
+let lastname = 'Farayola';
+let height = '5.65ft';
+let country = 'Nigeria';
+ console.log(firstname);
+ console.log(lastname);
+ console.log(height);
+ console.log(country);
+
+ let aboutMe = {
+    firstname: 'Tolulope',
+    lastname: 'Farayola',
+    height: '5.65ft',
+    country: 'Nigeria'
+ };
+ console.log(aboutMe);
